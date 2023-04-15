414fa4e0cc3812db686b594a9e8501ac3fd20162
dopeboy321 <105025755+dopeboy321@users.noreply.github.com>
2023-04-15 12:57:50 +0200

    reworked exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,14 +1,13 @@
-const removeFromArray = function(array, ...args) {
-
-    const myArray = [];
-    array.forEach((element) => {
-      if(!args.includes(element)) {
-        myArray.push(element)
+const removeFromArray = function(arr, ...args) {
+  const myArray = [];
+    arr.forEach((item) => {
+      if (!args.includes(item)) {
+        myArray.push(item)
       }
-    })
-    return myArray;
-};
-removeFromArray([1, 2, 3, 4], 3);
+  });
+  return myArray;
+}
 
+removeFromArray([1, 2, 3, 4], 3)
 // Do not edit below this line
 module.exports = removeFromArray;
